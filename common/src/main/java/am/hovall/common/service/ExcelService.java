50db627402ab1ba0b059cf960c180145e064461c
SevoMkrtchyan <sevo-mkrtchyan@mail.ru>
2021-10-23 23:26:42 +0400

    SBN-1008

--- common/src/main/java/am/hovall/common/service/ExcelService.java
+++ common/src/main/java/am/hovall/common/service/ExcelService.java
@@ -2,7 +2,12 @@ package am.hovall.common.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
 public interface ExcelService {
 
     void excelToProduct(MultipartFile[] files);
+
+    ByteArrayInputStream exportProducts() throws IOException;
 }
--- common/src/main/java/am/hovall/common/service/impl/ExcelServiceImpl.java
+++ common/src/main/java/am/hovall/common/service/impl/ExcelServiceImpl.java
@@ -10,20 +10,19 @@ import am.hovall.common.repository.ProductCategoryRepository;
 import am.hovall.common.repository.ProductRepository;
 import am.hovall.common.service.ExcelService;
 import lombok.RequiredArgsConstructor;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
 import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Locale;
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static org.apache.poi.ss.usermodel.IndexedColors.GREY_25_PERCENT;
 
 @Service
 @RequiredArgsConstructor
@@ -66,7 +65,12 @@ public class ExcelServiceImpl implements ExcelService {
                         Cell currentCell = cellsInRow.next();
                         switch (cellIdx) {
                             case 0:
-                                product.setBarcode((long) currentCell.getNumericCellValue());
+                                long barcodeFromFile = (long) currentCell.getNumericCellValue();
+                                Optional<Product> byBarcode = productRepository.findByBarcode(barcodeFromFile);
+                                if (byBarcode.isPresent()) {
+                                    product = byBarcode.get();
+                                }
+                                product.setBarcode(barcodeFromFile);
                                 break;
                             case 1:
                                 product.setTitle(currentCell.getStringCellValue());
@@ -81,31 +85,46 @@ public class ExcelServiceImpl implements ExcelService {
                                 product.setWeight(currentCell.getNumericCellValue());
                                 break;
                             case 5:
-                                MadeInCountry countryByTitle = madeInCountryRepository.findByTitle(currentCell.getStringCellValue().toLowerCase(Locale.ROOT));
+                                String madeInCountry = currentCell.getStringCellValue().toLowerCase(Locale.ROOT);
+                                if (madeInCountry.isEmpty() || madeInCountry.isBlank()) {
+                                    break;
+                                }
+                                MadeInCountry countryByTitle = madeInCountryRepository.findByTitle(madeInCountry);
                                 if (countryByTitle != null) {
                                     product.setMadeInCountry(countryByTitle);
                                 } else {
-                                    MadeInCountry savedCountry = madeInCountryRepository.save(new MadeInCountry(currentCell.getStringCellValue().toLowerCase(Locale.ROOT)));
+                                    MadeInCountry savedCountry = madeInCountryRepository.save(new MadeInCountry(madeInCountry));
                                     product.setMadeInCountry(savedCountry);
                                 }
                                 break;
                             case 6:
-                                ProductCategory byTitle = productCategoryRepository.findByTitle(currentCell.getStringCellValue().toLowerCase(Locale.ROOT));
+                                String productCategory = currentCell.getStringCellValue().toLowerCase(Locale.ROOT);
+                                if (productCategory.isEmpty() || productCategory.isBlank()) {
+                                    break;
+                                }
+                                ProductCategory byTitle = productCategoryRepository.findByTitle(productCategory);
                                 if (byTitle != null) {
                                     product.setProductCategory(byTitle);
                                 } else {
-                                    ProductCategory savedProduct = productCategoryRepository.save(new ProductCategory(currentCell.getStringCellValue().toLowerCase(Locale.ROOT)));
+                                    ProductCategory savedProduct = productCategoryRepository.save(new ProductCategory(productCategory));
                                     product.setProductCategory(savedProduct);
                                 }
                                 break;
                             case 7:
-                                Brand brandFromDb = brandRepository.findByTitle(currentCell.getStringCellValue().toLowerCase(Locale.ROOT));
+                                String brand = currentCell.getStringCellValue().toLowerCase(Locale.ROOT);
+                                if (brand.isEmpty() || brand.isBlank()) {
+                                    break;
+                                }
+                                Brand brandFromDb = brandRepository.findByTitle(brand);
                                 if (brandFromDb != null) {
                                     product.setBrand(brandFromDb);
                                 } else {
-                                    Brand savedBrand = brandRepository.save(new Brand(currentCell.getStringCellValue().toLowerCase(Locale.ROOT)));
+                                    Brand savedBrand = brandRepository.save(new Brand(brand));
                                     product.setBrand(savedBrand);
                                 }
+                                break;
+                            case 8:
+                                product.setCreatedDateTime(LocalDateTime.parse(currentCell.getStringCellValue()));
                                 break;
                             default:
                                 break;
@@ -120,6 +139,105 @@ public class ExcelServiceImpl implements ExcelService {
                 throw new RuntimeException("failed to parse Excel file: " + e.getMessage());
             }
         }
+    }
+
+    @Override
+    public ByteArrayInputStream exportProducts() throws IOException {
+        List<Product> productList = productRepository.findAll();
+        if (productList == null) {
+            throw new NullPointerException();
+        }
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Products");
+
+        CellStyle cellStyle = workbook.createCellStyle();
+        cellStyle.setFillBackgroundColor(GREY_25_PERCENT.getIndex());
+        cellStyle.setFillPattern(FillPatternType.BIG_SPOTS);
+
+        Row productsHeaderRow = sheet.createRow(0);
+        productsHeaderRow.setRowStyle(cellStyle);
+
+        Cell headerRowCell = productsHeaderRow.createCell(0);
+        headerRowCell.setCellValue("ID");
+        headerRowCell = productsHeaderRow.createCell(1);
+        headerRowCell.setCellValue("Շտրիխկոդ");
+        headerRowCell = productsHeaderRow.createCell(2);
+        headerRowCell.setCellValue("Անվանում");
+        headerRowCell = productsHeaderRow.createCell(3);
+        headerRowCell.setCellValue("Նկարագրություն");
+        headerRowCell = productsHeaderRow.createCell(4);
+        headerRowCell.setCellValue("Գին");
+        headerRowCell = productsHeaderRow.createCell(5);
+        headerRowCell.setCellValue("Քաշ/քանակ");
+        headerRowCell = productsHeaderRow.createCell(6);
+        headerRowCell.setCellValue("Արտադրման Երկիր");
+        headerRowCell = productsHeaderRow.createCell(7);
+        headerRowCell.setCellValue("Կատեգորիա");
+        headerRowCell = productsHeaderRow.createCell(8);
+        headerRowCell.setCellValue("Ապրանքանիշը");
+        headerRowCell = productsHeaderRow.createCell(9);
+        headerRowCell.setCellValue("Արտադրման ամսաթիվը");
+
+        int count = 1;
+        for (Product product : productList) {
+            Row productDataRow = sheet.createRow(sheet.getLastRowNum() + 1);
+            Cell productDataCell = productDataRow.createCell(0);
+            productDataCell.setCellValue(count);
+            productDataCell = productDataRow.createCell(1);
+            productDataCell.setCellValue(product.getBarcode());
+            productDataCell = productDataRow.createCell(2);
+            if (product.getTitle() != null) {
+                productDataCell.setCellValue(product.getTitle());
+            } else {
+                productDataCell.setBlank();
+            }
+            productDataCell = productDataRow.createCell(3);
+            if (product.getDescription() != null) {
+                productDataCell.setCellValue(product.getDescription());
+            } else {
+                productDataCell.setBlank();
+            }
+            productDataCell = productDataRow.createCell(4);
+            if (product.getProductCategory() != null) {
+                productDataCell.setCellValue(product.getPrice());
+            } else {
+                productDataCell.setBlank();
+            }
+            productDataCell = productDataRow.createCell(5);
+            if (product.getWeight() != 0) {
+                productDataCell.setCellValue(product.getWeight());
+            } else {
+                productDataCell.setBlank();
+            }
+            productDataCell = productDataRow.createCell(6);
+            if (product.getMadeInCountry() != null) {
+                productDataCell.setCellValue(product.getMadeInCountry().getTitle());
+            } else {
+                productDataCell.setBlank();
+            }
+            productDataCell = productDataRow.createCell(7);
+            if (product.getProductCategory() != null) {
+                productDataCell.setCellValue(product.getProductCategory().getTitle());
+            } else {
+                productDataCell.setBlank();
+            }
+            productDataCell = productDataRow.createCell(8);
+            if (product.getBrand() != null) {
+                productDataCell.setCellValue(product.getBrand().getTitle());
+            } else {
+                productDataCell.setBlank();
+            }
+            productDataCell = productDataRow.createCell(9);
+            if (product.getCreatedDateTime() != null) {
+                productDataCell.setCellValue(product.getCreatedDateTime().toString());
+            } else {
+                productDataCell.setBlank();
+            }
+            count += 1;
+        }
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        workbook.write(outputStream);
+        return new ByteArrayInputStream(outputStream.toByteArray());
     }
 
     private boolean hasExcelFormat(MultipartFile file) {
