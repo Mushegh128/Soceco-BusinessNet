209a7bc13e8aff435908be727273cefa676da92e
Arman Mkrtchyan <arman2027@icloud.com>
2021-11-10 18:28:12 +0400

    add search for order

--- common/src/main/java/am/hovall/common/response/CompanyResponse.java
+++ common/src/main/java/am/hovall/common/response/CompanyResponse.java
@@ -3,13 +3,12 @@ package am.hovall.common.response;
 import am.hovall.common.entity.CompanyType;
 import am.hovall.common.entity.Discount;
 import am.hovall.common.entity.PresSeller;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+@Data
 @Builder
-@Getter
-@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CompanyResponse {
     private Long id;
     private String name;
--- common/src/main/java/am/hovall/common/service/OrderService.java
+++ common/src/main/java/am/hovall/common/service/OrderService.java
@@ -1,12 +1,24 @@
 package am.hovall.common.service;
 
 import am.hovall.common.entity.OrderStatus;
+import am.hovall.common.exception.OrderNotFoundException;
 import am.hovall.common.request.OrderRequest;
 import am.hovall.common.response.OrderResponse;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OrderService {
+
+    List<OrderResponse> findAllByUserIdAndCompanyId(long userId, long companyId);
+
+    List<OrderResponse> findAllUnSynchronized() throws OrderNotFoundException;
+
+    List<OrderResponse> findAllByOrderStatus(OrderStatus orderStatus);
+
+    List<OrderResponse> findAllByDateRange(String start, String end);
+
+    OrderResponse findBySerialNumber(long serialNumber) throws OrderNotFoundException;
 
     Double getCompanyDebt(long registerNumber);
 
