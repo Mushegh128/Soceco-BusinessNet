95ba3374902fadcf36cb82fa55e0e61b1a0a29ed
Mushegh128 <thesoceco@gmail.com>
2021-11-11 18:05:45 +0400

    SBN-[2001] add payment structure

--- common/src/main/java/am/hovall/common/service/PaymentService.java
+++ common/src/main/java/am/hovall/common/service/PaymentService.java
@@ -2,6 +2,7 @@ package am.hovall.common.service;
 
 import am.hovall.common.entity.PaymentStatus;
 import am.hovall.common.request.PaymentRequest;
+import am.hovall.common.request.PaymentsSearchRequest;
 import am.hovall.common.response.PaymentResponse;
 
 import java.util.List;
@@ -17,4 +18,6 @@ public interface PaymentService {
     PaymentResponse setPaymentStatusBySerialNumber(PaymentStatus paymentStatus, String serialNumber);
 
     List<PaymentResponse> findByStatus(PaymentStatus created);
+
+    List<PaymentResponse> search(PaymentsSearchRequest paymentsSearchRequest);
 }
---
+++ web/src/main/java/am/hovall/web/controller/PaymentController.java
@@ -0,0 +1,54 @@
+package am.hovall.web.controller;
+
+import am.hovall.common.entity.PaymentStatus;
+import am.hovall.common.request.PaymentsSearchRequest;
+import am.hovall.common.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("payments")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @GetMapping
+    public String payments(ModelMap modelMap){
+        modelMap.addAttribute("payments", paymentService.findByStatus(PaymentStatus.CREATED));
+        modelMap.addAttribute("paymentsSearchRequest",new PaymentsSearchRequest());
+        return "payments";
+    }
+
+    @PostMapping("/{serialNumber}/{status}")
+    public String status(@PathVariable("serialNumber") String serialNumber, @PathVariable("status") int status){
+        if (status == 1){
+            paymentService.setPaymentStatusBySerialNumber(PaymentStatus.CONFIRMED, serialNumber);
+        }else if (status == 0){
+            paymentService.setPaymentStatusBySerialNumber(PaymentStatus.DENIED, serialNumber);
+        }
+        return "redirect:/payments";
+    }
+
+    @GetMapping("/{registerNumber}")
+    public String byCompany(@PathVariable("registerNumber") String registerNumber, ModelMap modelMap){
+        modelMap.addAttribute("payments", paymentService.findAllByCompanyRegisterNumber(Long.parseLong(registerNumber)));
+        return "payments";
+    }
+
+    @GetMapping("/user/{id}")
+    public String byUser(@PathVariable("id") Long id, ModelMap modelMap){
+        modelMap.addAttribute("payments", paymentService.findAllByFromUser(id));
+        return "payments";
+    }
+
+    @PostMapping("/search")
+    public String search(ModelMap modelMap, @ModelAttribute PaymentsSearchRequest paymentsSearchRequest){
+        modelMap.addAttribute("payments", paymentService.search(paymentsSearchRequest));
+        return "payments";
+    }
+
+}
+
